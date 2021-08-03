3b59beeea580c8d52a303b9dcb2b2a3c3fb9d2ac
Mayur Parmar <mayur.parmar@epistic.com>
2021-08-03 10:31:28 +0530

    MayurParmar: create folder od today date

---
+++ 02-08-2021/MayurParmar/index.js
@@ -0,0 +1,20 @@
+for (var i = 1; i <= 10; i++) {
+  var a = 10;
+  console.log(a + " * " + i + " = " + a * i);
+}
+
+var b = 20;
+while (a <= 20) {
+  console.log("mayur");
+}
+
+// var i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 10);
