673ac5e5f72584dddf885fbc4d09edb2948ef8ba
Mayur Parmar <mayur.parmar@epistic.com>
2021-08-04 10:30:59 +0530

    MayurParmar: "task is completed"

---
+++ 04-08-2021/MayurParmar/index.js
@@ -0,0 +1,12 @@
+let firstInteger = 4;
+let firstDecimal = 4.0;
+let firstString = "HackerRank";
+
+let secondInteger = "12";
+let secondDecimal = "4.32";
+let secondString =
+  " is the best place to learn and practice coding! , we get HackerRank is the best place to learn and practice coding! .";
+
+console.log(firstInteger + parseInt(secondInteger));
+console.log(firstDecimal + parseFloat(secondDecimal));
+console.log(firstString + secondString);
