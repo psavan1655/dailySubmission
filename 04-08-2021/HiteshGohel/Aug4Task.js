b1fc02c53cf42712b74592137dadbe93414d24a5
Hitesh Gohel <hitesh.gohel.epistic@gmail.com>
2021-08-04 11:36:37 +0530

    Hitesh Gohel - Javascript DataType Example

---
+++ 04-08-2021/HiteshGohel/Aug4Task.js
@@ -0,0 +1,19 @@
+const firstInteger = 4
+const firstDecimal = 4.0
+const firstString = 'HackerRank'
+
+function dataType (secondInteger, secondDecimal, secondString) {
+  return `${firstInteger + secondInteger};\n${firstDecimal +
+    secondDecimal};\n${firstString + secondString}`
+}
+
+// function dataType (secondInteger, secondDecimal, secondString) {
+//   const IntAddition = firstInteger + secondInteger
+//   const FloatAddition = firstDecimal + secondDecimal
+//   const StringConcatenation = firstString + secondString
+//   return `${IntAddition};\n${FloatAddition};\n${StringConcatenation}`
+// }
+
+console.log(
+  dataType(12, 4.32, 'is the best place to learn and practice coding!')
+)
