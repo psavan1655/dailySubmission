6f5da2cfb10498a1fd1b530711282bf58cf2ef6a
harshil prajapati <harshil.prajapati.epistic@gmail.com>
2021-08-04 18:57:11 +0530

    make a demo of javascripts as i learned

---
+++ 04-08-2021/Harshil/datatype.js
@@ -0,0 +1,8 @@
+let a=149864565653646598788470868n;
+let b="harshil";
+let c=[];
+console.log(typeof(c));
+console.log(Array.isArray(c));
+
+let checknumber = 5 > 1;
+console.log(checknumber);
---
+++ 04-08-2021/Harshil/expo.js
@@ -0,0 +1,10 @@
+let number = (12 ** 2);
+console.log(number);
+console.log("harshil" - "prajapati");
+console.log(true + true);
+console.log(false + false);
+console.log(5 - "10");
+
+
+let num =12;
+console.log(-num);
