00116b390a3219068e452d00a6284c6e27cbf91d
richa1204 <richakothari12@gmail.com>
2021-08-04 12:16:24 +0530

    richa:javascripttask

---
+++ 04-08-2021/richa/richa.js
@@ -0,0 +1,16 @@
+function dataType(secondInteger, secondDecimal, secondString)
+{
+let firstInteger = 4;
+let firstDecimal = 4.0;
+let firstString = "hackerrank";
+
+
+return `${firstInteger + secondInteger};\n${firstDecimal +
+    secondDecimal};\n${firstString + secondString}`
+}
+
+
+console.log(
+    dataType(12, 4.32, "is the best place to learn and practice coding!")
+  );
+  
