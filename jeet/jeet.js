53044ded22b230dfdeeff5be04124ea6a9ac4baa
STRANGER\Jeet Patel <jeetp6701@gmail.com>
2021-08-06 08:40:33 -1100

    jeet-javascript

--- jeet/jeet.js
+++ jeet/jeet.js
@@ -1,139 +1,35 @@
-dataType(12, 4.32, "is the best place to learn and practice coding!")
-function dataType(secondInteger, secondDecimal, secondString) {
-    const firstInteger = 4
-    const firstDecimal = 4.0
-    const firstString = "HackerRank "
-    return console.log(firstInteger + secondInteger, firstDecimal + secondDecimal , firstString + secondString)
-}
+// // // let a=10;""
+// // // console.log("ghanu")
+// // // let a="ghanu2"
+// // // console.log(a)
+// // // let b="ghanubhabhi(harsh vala)"
+// // // console.log(b)
 
+// // // let a=10
+// // // let b=20
+// // // console.log(a);
 
-function dataType(secondInteger, secondDecimal, secondString) {
-    const firstInteger = 4
-    const firstDecimal = 4.0
-    const firstString = "HackerRank "
-  
-  //   // Return your output here.
-  
-  return `${firstInteger + secondInteger};\n${firstDecimal +
-      secondDecimal};\n${firstString + secondString}`
-  }
-  // It should display the expected output.
-  console.log(
-    dataType(12, 4.32, "is the best place to learn and practice coding!")
-  )
-  
+// // var a=10
+// // var b=13
+// // console.log("ghanu");
 
+// let a10;
+// a=10
+// a10=20
+// console.log(a10);
+// "use strict"
+// num=4
+// console.log(num)
+// let ghanubhabhi="1"
+// , ghanubhabhi2="2"
+// ,ghanubhabhi3="3"
 
-// class classname {
-//     constructor(name,age){
-//         this.name,(this.name = name);
-//         this.age,(this.age = age);
-//     }
-//     display(){
-//         console.log('this is object =>', this );
-//     }
-// }
-// let obj1 = new classname('abc',20);
-// let obj2 = new classname('def',21);
-// let obj3 = new classname('ghi',22);
-// {
-//     console.log(obj1.display());
-//     console.log(obj2.display());
-//     console.log(obj3.display());
-// }
+// console.log(ghanubhabhi,ghanubhabhi2,ghanubhabhi3);
 
-// var obj = {
-//     name : 'xyz',
-//     surname : 'abc',
-//     age : 22,
-//     fullinfo:function() {
-//         return this.name + '' +this.surname + ''+this.age;
-//     }
-// }
-// console.log(obj.fullinfo());
+// const COLOR = "ghanub/habhi4"
+// let colors= COLOR;
+// console.log(colors);
+console.log(1/0)
 
-
-// function primenum(a){
-//     for(i=2;i<=a/2;i++){
-//         if(a%i==0){
-//             console.log('this is not prime number');
-//             return false;
-//         }
-//        }return true;
-//     }
-
-// console.log(primenum(15));
-
-// let armstrong = (x) => {
-//     let temp = x;
-//     let sum = 0;
-//     while(temp != 0){
-//         rem = temp % 10;
-//         sum = sum + (rem ** (x.toString()).length);   
-//         temp=parseInt(temp/10)
-//         // temp = Math.floor(temp/10)
-//     }
-// return sum==x;
-// }
-// console.log(armstrong(153));
-
-// function fibo(a){
-//     if(a == 1){
-//         return [0,1];
-//     }else{
-//         let arr = fibo(a-1);
-//         arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
-//         return arr;
-//     }
-// }
-// console.log(fibo(8));
-
-// let primenum = (a) => {
-//     for(i=2; i<=a/2; i++){
-//     if(a%i==0){
-//         console.log('this is not a prime number');
-//         return false;
-//     }else{
-//         console.log('this is a prime number');
-//         return true;
-//     }
-// }
-// }
-// console.log(primenum(15));
-
-// class test {
-//     constructor(name,age){
-//         this.name,(this.name = name);
-//         this.age,(this.age = age);
-//     }
-//     display(){
-//         console.log('this is a test class',this);
-//     }
-// }
-// let test1 = new test('abc',52);
-// let test2 = new test('xyz',15);
-// {
-//     console.log(test1.display(),test2.display());
-// }
-
-// obj = {
-//     name:'xyz',
-//     age:25,
-//     date:21,
-//     fullinfo:function() {
-//         return this.name+this.age+this.date;
-//     }
-// }
-// console.log(obj.fullinfo());
-
-// let armstrongnum = function (a){
-//         let temp = a;
-//         sum = 0;
-//         while (temp != 0){
-//             rem = temp % 10;
-//             sum=sum + (rem**(a.toString()).length);
-//             temp=Math.floor(temp/10)
-//         }
-//         return sum==a;
-// }
-// console.log(armstrongnum(524));
+const bigInt = 1234567890123456789012345678901234567890
+console.log(bigInt);
