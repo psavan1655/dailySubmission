a6d1ee01f7160132599413c9f10f69d320cfefc5
Mayur Parmar <mayur.parmar@epistic.com>
2021-08-06 09:43:08 +0530

    MayurParmar: 1.0 to 2.6 demo

---
+++ 04-08-2021/MayurParmar/js/js1.js
@@ -0,0 +1,51 @@
+// alert("I'm JavaScript");
+// let i = 0;
+// while (i < 10) {
+//   console.log("fdf");
+// }
+
+// console.log(1 / 0);
+// console.log(Infinity);
+// console.log("not a number" / 2);
+// console.log("not a number" / 2 + 5);
+
+// solution 1
+// Declare two variables: admin and name.
+// Assign the value "John" to name.
+// Copy the value from name to admin.
+// Show the value of admin using alert (must output “John”).
+// let admin;
+// let name = "john";
+// admin = name;
+// console.log(admin);
+
+// solution 2
+// Create a variable with the name of our planet. How would you name such a variable?
+// Create a variable to store the name of a current visitor to a website. How would you name that variable?
+// let ourPlanetName = "Earth";
+// let currentVisitorName = "Mayur Parmar";
+// console.log(ourPlanetName + "\n" + currentVisitorName);
+
+// console.log(`hello ${1}`);
+// console.log(`hello ${"name"}`);
+// let name = "mayur";
+// console.log(`hello ${name}`);
+
+let a = undefined;
+console.log(typeof a);
+a = 0;
+console.log(typeof a);
+a = 10n;
+console.log(typeof a);
+a = true;
+console.log(typeof a);
+a = "mayur";
+console.log(typeof a);
+a = Symbol("id");
+console.log(typeof a);
+a = Math;
+console.log(typeof a);
+a = null;
+console.log(typeof a);
+a = alert();
+console.log(typeof a);
