3169cea218874749455af341d848180629496cc3
Hitesh Gohel <hitesh.gohel.epistic@gmail.com>
2021-08-19 19:13:58 +0530

    Hitesh Gohel - Everithing is Done in Form

--- 19-08-2021/HiteshGohel/ReactForm19Aug/src/App.js
+++ 19-08-2021/HiteshGohel/ReactForm19Aug/src/App.js
@@ -1,7 +1,6 @@
 import './App.css'
 import TaskForm from './TaskForm'
 // import Test from './Test'
-import DataDisplay from './DataDisplay'
 import { Switch, Route } from 'react-router-dom'
 
 const App = () => {
@@ -9,7 +8,6 @@ const App = () => {
     <div className="App">
       <Switch>
         <Route exact path="/" component={TaskForm} />
-        <Route exact path="/data" component={DataDisplay} />
       </Switch>
       {/* <Test /> */}
     </div>
